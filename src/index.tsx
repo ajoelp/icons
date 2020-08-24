ff6d662130ac5c709edc2db320562388ae598acc
Joel Podrebarac <joelpodrebar@gmail.com>
2020-08-24 16:55:13 -0400

    Workflows

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,19 @@ interface BaseProps {
 }
 
 interface SolidProps extends BaseProps{
-  variant: 'solid'
+  variant?: 'solid'
   icon: keyof typeof SolidIcons
 }
 
 interface OutlineProps extends BaseProps {
-  variant: 'outline'
+  variant?: 'outline'
   icon: keyof typeof OutlineIcons;
 }
 
 export type Props = SolidProps | OutlineProps
 
-export function Icon(props: Props){
-  const icons = props.variant === 'solid' ? SolidIcons : OutlineIcons;
-  const Icon = icons[props.icon];
-  return <Icon className={props.className} />
+export function Icon({variant = "solid", icon, className}: Props){
+  const icons = variant === 'solid' ? SolidIcons : OutlineIcons;
+  const Icon = icons[icon];
+  return <Icon className={className} />
 }
