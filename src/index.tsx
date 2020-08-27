ce71dcb4c2162b60bb3ffa3a5c1d9208cb79fade
Joel Podrebarac <joelpodrebar@gmail.com>
2020-08-27 15:53:00 -0400

    Lint and types

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,27 @@ import * as React from 'react';
 import * as OutlineIcons from './icons/outline';
 import * as SolidIcons from './icons/solid';
 
+export type SolidIconsType = keyof typeof SolidIcons;
+export type OutlineIconsType = keyof typeof SolidIcons;
+
 interface BaseProps {
   className?: string;
 }
 
-interface SolidProps extends BaseProps{
-  variant?: 'solid'
-  icon: keyof typeof SolidIcons
+interface SolidProps extends BaseProps {
+  variant?: 'solid';
+  icon: SolidIconsType;
 }
 
 interface OutlineProps extends BaseProps {
-  variant?: 'outline'
-  icon: keyof typeof OutlineIcons;
+  variant?: 'outline';
+  icon: OutlineIconsType;
 }
 
-export type Props = SolidProps | OutlineProps
+export type Props = SolidProps | OutlineProps;
 
-export function Icon({variant = "solid", icon, className}: Props){
+export function Icon({ variant = 'solid', icon, className }: Props) {
   const icons = variant === 'solid' ? SolidIcons : OutlineIcons;
   const Icon = icons[icon];
-  return <Icon className={className} />
+  return <Icon className={className} />;
 }
